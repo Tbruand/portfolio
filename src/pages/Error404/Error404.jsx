44316961c71935847f788deda72913d0b4a89168
Thomas Bruand <thomas.bruand@gmail.com>
2024-05-24 10:02:05 +0200

    Ajout du contenu de la page Error404, avec un Link pour rediriger vers le '/'

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-// import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Error404() {
   return (
     <>
-      
+      <div className='errorPage'>
+        <p>Error</p>
+        <p>404</p>
+        <Link className='errorBtn' to='/'>
+          <button>Retour à l'accueil !</button>
+          <span className="spanLight"></span>
+        </Link>
+      </div>
     </>
   );
 }
