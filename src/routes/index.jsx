be2d73d9c8eb67a9e6b900d373c037be5a34f136
Thomas Bruand <thomas.bruand@gmail.com>
2024-05-24 10:04:16 +0200

    Ajout de la route pour stack

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,22 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/home";
+import Stack from "../pages/Stack/Stack";
 import Error404 from "../pages/Error404/Error404";
 import LayoutDefault from "../layouts/Default/Default";
 import LayoutBlank from "../layouts/Blank/Blank";
+import LayoutUnGrid from "../layouts/UnGrid/UnGrid";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<LayoutDefault />}>
-          <Route path='/' element={<Home />} />
+        <Route path="/" element={<LayoutDefault />}>
+          <Route index element={<Home />} />
+        </Route>
+
+        <Route element={<LayoutUnGrid />}>
+          <Route path='stacks' element={<Stack/>} />
         </Route>
 
         <Route element={<LayoutBlank />}>
