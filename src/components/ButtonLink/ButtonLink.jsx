29b21d897a53d7583883d0a604a3a0414a421b19
Thomas Bruand <thomas.bruand@gmail.com>
2024-05-24 09:58:44 +0200

    Mise en place de la const buttonLinks pour boucler sur le component ButtonLinkItem

--- src/components/ButtonLink/ButtonLink.jsx
+++ src/components/ButtonLink/ButtonLink.jsx
@@ -1,51 +1,39 @@
 import React from "react";
+import ButtonLinkItem from "../ButtonLinkItem/ButtonLinkItem";
 
 function ButtonLink() {
+  const buttonLinks = [
+    {
+      href: "https://dribbble.com/shots/23181733-Blurry-Glassmorphic-Modern-Landing-page",
+      className: "button first",
+      label: "À propos de moi",
+    },
+    {
+      href: "https://youtu.be/1pW_sk-2y40",
+      className: "button second",
+      label: "Stacks",
+    },
+    {
+      href: "https://x.com/juxtopposed",
+      className: "button third",
+      label: "Projets",
+    },
+    {
+      href: "https://x.com/juxtopposed",
+      className: "button fourth",
+      label: "Contact",
+    },
+  ];
   return (
     <>
-      {/* first btn */}
-      <a
-        href='https://dribbble.com/shots/23181733-Blurry-Glassmorphic-Modern-Landing-page'
-        target='_blank'
-        rel='noreferrer'
-        class='button first'
-      >
-        <button>À propos de moi</button>
-        <span></span>
-      </a>
-
-      {/* second btn */}
-      <a
-        href='https://youtu.be/1pW_sk-2y40'
-        target='_blank'
-        rel='noreferrer'
-        class='button second'
-      >
-        <button>Stacks</button>
-        <span></span>
-      </a>
-
-      {/* third btn */}
-      <a
-        href='https://x.com/juxtopposed'
-        target='_blank'
-        rel='noreferrer'
-        class='button third'
-      >
-        <button>Projets</button>
-        <span></span>
-      </a>
-
-      {/* fourth btn */}
-      <a
-        href='https://x.com/juxtopposed'
-        target='_blank'
-        rel='noreferrer'
-        class='button fourth'
-      >
-        <button>Contact</button>
-        <span></span>
-      </a>
+      {buttonLinks.map((link, index) => (
+        <ButtonLinkItem
+          key={index}
+          href={link.href}
+          className={link.className}
+          label={link.label}
+        />
+      ))}
     </>
   );
 }
