bcb8b83f2e79ce492ff9d1bb07fb75aeb0ac66cc
Thomas Bruand <thomas.bruand@gmail.com>
2024-05-24 10:00:46 +0200

    Ajout des svg dans les layouts

--- src/layouts/Blank/Blank.jsx
+++ src/layouts/Blank/Blank.jsx
@@ -1,10 +1,16 @@
 // layout pour la page Error404
 import { Outlet } from "react-router-dom";
+import TopRightSvg from "../../components/SvgBackground/TopRightSvg";
+import BottomLeftSvg from "../../components/SvgBackground/BottomLeftSvg";
+import GridSvg from "../../components/SvgBackground/GridSvg";
 
 function Blank() {
   return (
     <>
       <Outlet />
+      <GridSvg />
+      <TopRightSvg />
+      <BottomLeftSvg />
     </>
   );
 }
--- src/layouts/Default/Default.jsx
+++ src/layouts/Default/Default.jsx
@@ -1,10 +1,18 @@
-// Ajout header / footer
+import React from "react";
 import { Outlet } from "react-router-dom";
+import Navigation from '../../components/Navigation/Navigation';
+import TopRightSvg from "../../components/SvgBackground/TopRightSvg";
+import BottomLeftSvg from "../../components/SvgBackground/BottomLeftSvg";
+import GridSvg from "../../components/SvgBackground/GridSvg";
 
 function Default() {
   return (
     <>
+      <Navigation />
       <Outlet />
+      <GridSvg />
+      <TopRightSvg />
+      <BottomLeftSvg />
     </>
   );
 }
