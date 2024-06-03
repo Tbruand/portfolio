bd2ad56551b0017c6cb94cbd67af0c847aa411ee
Thomas Bruand <thomas.bruand@gmail.com>
2024-06-03 07:43:13 +0200

    ajout de <main>

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 function Error404() {
   return (
     <>
-      <div className='errorPage'>
-        <p>Error</p>
-        <p>404</p>
-        <Link className='errorBtn' to='/'>
-          <button>Retour à l'accueil !</button>
-          <span className="spanLight"></span>
-        </Link>
-      </div>
+      <main>
+        <div className='errorPage'>
+          <p>Error</p>
+          <p>404</p>
+          <Link className='errorBtn' to='/'>
+            <button>Retour à l'accueil !</button>
+            <span className='spanLight'></span>
+          </Link>
+        </div>
+      </main>
     </>
   );
 }
