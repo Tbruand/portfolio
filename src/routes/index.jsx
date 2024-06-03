18790ed4f4aca5860fa072ad30a1d08ec0dc8dfd
Thomas Bruand <thomas.bruand@gmail.com>
2024-06-03 07:46:18 +0200

    ajout des routes manquantes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home/home";
-import Stack from "../pages/Stack/Stack";
-import Error404 from "../pages/Error404/Error404";
 import LayoutDefault from "../layouts/Default/Default";
 import LayoutBlank from "../layouts/Blank/Blank";
 import LayoutUnGrid from "../layouts/UnGrid/UnGrid";
+import Home from "../pages/Home/home";
+import About from "../pages/About/About";
+import Stack from "../pages/Stack/Stack";
+import Contact from "../pages/Contact/Contact";
+import Error404 from "../pages/Error404/Error404";
+import Project from "../pages/Project/Project";
+
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LayoutDefault />}>
+        <Route path='/' element={<LayoutDefault />}>
           <Route index element={<Home />} />
+          <Route path='contact' element={<Contact />} />
         </Route>
 
         <Route element={<LayoutUnGrid />}>
-          <Route path='stacks' element={<Stack/>} />
+          <Route path='stacks' element={<Stack />} />
+          <Route path='about' element={<About />} />
+          <Route path='projects' element={<Project />} />
         </Route>
 
         <Route element={<LayoutBlank />}>
