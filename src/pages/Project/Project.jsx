498455ecab60c79bc23a251610a2b1121e7dc75e
Thomas Bruand <thomas.bruand@gmail.com>
2024-06-03 07:30:28 +0200

    ajout de la page Project

---
+++ src/pages/Project/Project.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Carousel from "../../components/Carousel/Carousel";
+
+function Project() {  
+  return (
+    <>
+    <main>
+        <Carousel />
+      </main>
+      
+    </>
+  );
+}
+
+export default Project;
