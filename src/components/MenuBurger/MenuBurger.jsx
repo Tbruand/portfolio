0024b00d4dd37fc30d52b49c97859ed62121bfbc
Thomas Bruand <thomas.bruand@gmail.com>
2024-06-10 10:20:18 +0200

    ajout d'un menu burger pour mobile

--- src/components/MenuBurger/MenuBurger.jsx
+++ src/components/MenuBurger/MenuBurger.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 function MenuBurger() {
+  const [animate, setAnimate] = useState(false);
+  const [overlayVisible, setOverlayVisible] = useState(false);
+
+  const handleMenuClick = () => {
+    setAnimate(!animate);
+    setOverlayVisible(!overlayVisible);
+  };
+  const handleLinkClick = () => {
+    setAnimate(false);
+    setOverlayVisible(false);
+  };
   const itemNav = [
     {
       title: "À propos",
@@ -23,17 +34,27 @@ function MenuBurger() {
   ];
 
   return (
-    <nav>
-      <ul>
-        {itemNav.map((item, index) => (
-          <li key={index}>
-            <Link className='navLi' to={item.to}>
-              {item.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <nav className='menu'>
+        <div
+          className={`menuBurger ${animate ? "animate" : ""}`}
+          onClick={handleMenuClick}
+        >
+          <div className='bar'></div>
+        </div>
+        <div className={`overlay ${overlayVisible ? 'menuVisible' : ''}`}>
+          <ul>
+            {itemNav.map((item, index) => (
+              <li key={index}>
+                <Link className='navLi' to={item.to} onClick={handleLinkClick}>
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
 
