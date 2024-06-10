7b7a29a91943750fcfd96ede317042b6d9d7a22f
Thomas Bruand <thomas.bruand@gmail.com>
2024-06-10 10:19:37 +0200

    mise a jour de la syntaxe

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,19 +3,16 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 const projects = [
   {
     title: "Kasa",
-    description:
-      "Application web de location immobilière avec React.",
+    description: "Application web de location immobilière avec React.",
     imageUrl: "/assets/img/responsive_kasa.png",
     link: "https://github.com/BruandT/P_Kasa.git",
   },
   {
     title: "Dofus Match",
-    description:
-      "Application web de jeu de mémoire.",
+    description: "Application web de jeu de mémoire.",
     imageUrl: "/assets/img/dofus_match.png",
     link: "https://github.com/BruandT/dofus-match.git",
   },
@@ -40,18 +37,10 @@ function Carousel() {
     slidesToScroll: 1,
     appendDots: (dots) => (
       <div style={{}}>
-        <ul className="dotUl">
-          {dots}
-        </ul>
+        <ul className='dotUl'>{dots}</ul>
       </div>
     ),
-    customPaging: (i) => (
-      <div
-        className="dotCarousel"
-      >
-        {i + 1}
-      </div>
-    ),
+    customPaging: (i) => <div className='dotCarousel'>{i + 1}</div>,
     responsive: [
       {
         breakpoint: 1024,
@@ -77,9 +66,16 @@ function Carousel() {
                 className='carouselImage'
               />
               <div className='carouselContent'>
-                <h3 className="textColor">{project.title}</h3>
+                <h3 className='textColor'>{project.title}</h3>
                 <p>{project.description}</p>
-                <a className="textColor" target="_blank" rel="noreferrer" href={project.link}>Info +</a>
+                <a
+                  className='textColor'
+                  target='_blank'
+                  rel='noreferrer'
+                  href={project.link}
+                >
+                  Info +
+                </a>
               </div>
             </div>
           ))}
